bd3e4f6eaa389b7b9c1090ae1b3bab1c26df4f73
k3v1n <k3v1n0x90@gmail.com>
2019-05-23 21:02:40 +0200

    Rewrite resistor color duo without class

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -11,41 +11,24 @@ const RESISTOR_COLOR_VALUE_MAPPING = {
   white: 9
 }
 
-class ResistorDuo {
-  constructor(resistorColorPair) {
-    this.resistorColorPair = resistorColorPair
+function calculateColorResistance(resistance, resistorColor, colorIndex) {
+  return resistance * getDigitPlaceMultiplier(colorIndex) + getResistorColorValue(resistorColor)
+}
+
+function getDigitPlaceMultiplier(resistorColorIndex) {
+  return Math.pow(10, resistorColorIndex)
+}
+
+function getResistorColorValue(resistorColor) {
+  const value = RESISTOR_COLOR_VALUE_MAPPING[resistorColor]
+
+  if (value === undefined) {
+    throw new Error(`invalid resistor color ${resistorColor} does not exist`)
   }
 
-  calculateResistance() {
-    return this.resistorColorPair.reduce(ResistorDuo.calculateColorResistance, 0)
-  }
-
-  static calculateColorResistance(resistance, resistorColor, colorIndex) {
-    return resistance * ResistorDuo.getDigitPlaceMultiplier(colorIndex) + ResistorDuo.getResistorColorValue(resistorColor)
-  }
-
-  static isFirstResistanceColor(resistorColorIndex) {
-    return resistorColorIndex === 0
-  }
-
-  static getDigitPlaceMultiplier(resistorColorIndex) {
-    const FIRST_DIGIT = 1
-    const SECOND_DIGIT = 10
-
-    return ResistorDuo.isFirstResistanceColor(resistorColorIndex) ? FIRST_DIGIT : SECOND_DIGIT
-  }
-
-  static getResistorColorValue(resistorColor) {
-    const value = RESISTOR_COLOR_VALUE_MAPPING[resistorColor]
-
-    if (value === undefined) {
-      throw new Error(`invalid resistor color ${resistorColor} does not exist`)
-    }
-
-    return value
-  }
+  return value
 }
 
 export function value(resistorColorPair) {
-  return new ResistorDuo(resistorColorPair).calculateResistance()
+  return resistorColorPair.reduce(calculateColorResistance, 0)
 }
