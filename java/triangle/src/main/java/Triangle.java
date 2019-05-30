8d5a6171cf5ba6ef534b9733b579f282b0deaee5
k3v1n <k3v1n0x90@gmail.com>
2019-05-30 15:26:44 +0200

    Second iteration of java triangle

--- java/triangle/src/main/java/Triangle.java
+++ java/triangle/src/main/java/Triangle.java
@@ -1,33 +1,53 @@
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+
 class Triangle {
 
-    private final double side1;
-    private final double side2;
-    private final double side3;
+    private final Map<Double, List<Double>> groupedSides;
 
     Triangle(final double side1, final double side2, final double side3) throws TriangleException {
-        if (side1 == 0d && side2 == 0d && side3 == 0d) {
+        groupedSides = DoubleStream.of(side1, side2, side3)
+                .boxed()
+                .collect(Collectors.groupingBy(Function.identity()));
+
+        if (allSidesAreZero()) {
             throw new TriangleException("side1, side2 and side3 must not be 0");
         }
 
-        if (side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1) {
+        if (!hasTriangleInequality()) {
             throw new TriangleException("triangle inequality not fulfilled");
         }
+    }
 
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
+    private boolean hasTriangleInequality() {
+        final var sidesDescending = groupedSides.values()
+                .stream()
+                .flatMap(Collection::stream)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        return sidesDescending.get(0) <= sidesDescending.get(1) + sidesDescending.get(2);
+    }
+
+    private boolean allSidesAreZero() {
+        return isEquilateral() && groupedSides.containsKey(0d);
     }
 
     boolean isEquilateral() {
-        return side1 == side2 && side2 == side3;
+        return groupedSides.size() == 1;
     }
 
     boolean isIsosceles() {
-        return side1 == side2 || side1 == side3 || side2 == side3;
+        return isEquilateral() || groupedSides.size() == 2;
     }
 
     boolean isScalene() {
-        return side1 != side2 && side1 != side3 && side2 != side3;
+        return !isIsosceles();
     }
 
 }
