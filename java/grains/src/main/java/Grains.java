c1dcb5d1645e9760e8f731e3fee983b33025b1b5
k3v1n <k3v1n0x90@gmail.com>
2021-04-03 14:21:39 +0200

    Solve java grains

---
+++ java/grains/src/main/java/Grains.java
@@ -0,0 +1,30 @@
+import java.math.BigInteger;
+
+class Grains {
+
+    private static final BigInteger TOTAL_GRAINS = new BigInteger("18446744073709551615", 10);
+
+    BigInteger grainsOnSquare(final int square) {
+        sanitizeSquare(square);
+
+        // Grains are powers of two so the following applies:
+        // 32 16 8 4 2 1
+        // 0  0  0 0 0 1 << 0 = 1
+        // 0  0  0 0 1 0 << 1 = 2
+        // 0  0  0 1 0 0 << 2 = 4
+        return BigInteger.valueOf(1L).shiftLeft(square - 1);
+    }
+
+    private static void sanitizeSquare(final int square) {
+        final boolean isValidSquare = square >= 1 && square <= 64;
+
+        if (!isValidSquare) {
+            throw new IllegalArgumentException("square must be between 1 and 64");
+        }
+    }
+
+    BigInteger grainsOnBoard() {
+        return TOTAL_GRAINS;
+    }
+
+}
