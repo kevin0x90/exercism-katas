4f19a899d0a50dcbe9d21a9d7ee6ea433099d5a7
k3v1n <k3v1n0x90@gmail.com>
2021-04-26 20:39:55 +0200

    Update javascript matrix tests and solution

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,11 +1,9 @@
-function parseRows(rowStrings) {
-  return rowStrings.map(row => row.split(' ').map(elem => parseInt(elem, 10)))
-}
+const parseRows = (rowStrings) => rowStrings.map(row => row.split(' ').map(elem => parseInt(elem, 10)))
 
 function generateColumns(maxNumberOfColumns, rows) {
   const columns = []
   for (let i = 0; i < maxNumberOfColumns; ++i) {
-    columns[i] = rows.map(row => row[i])
+    columns.push(rows.map(row => row[i]))
   }
 
   return columns
@@ -14,7 +12,7 @@ function generateColumns(maxNumberOfColumns, rows) {
 function parseMatrix(matrixDescription) {
   const rowStrings = matrixDescription.split('\n')
   const rows = parseRows(rowStrings)
-  const maxNumberOfColumns = rows.reduce((result, row) => Math.max(result, row.length), 0)
+  const maxNumberOfColumns = rows[0].length
   const columns = generateColumns(maxNumberOfColumns, rows)
 
   return {
