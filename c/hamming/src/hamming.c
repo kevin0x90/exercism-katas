b98c2bcf2efcea7baba337513e8845694c47e69e
k3v1n <k3v1n0x90@gmail.com>
2021-05-01 14:07:31 +0200

    Solve c hamming distance

--- c/hamming/src/hamming.c
+++ c/hamming/src/hamming.c
@@ -1,1 +1,24 @@
 #include "hamming.h"
+#include <stdbool.h>
+
+static inline bool is_end_of_string(const char *str)
+{
+  return *str == '\0';
+}
+
+int compute(const char *lhs, const char *rhs)
+{
+  int difference = 0;
+
+  for (; !is_end_of_string(lhs) && !is_end_of_string(rhs); ++lhs, ++rhs)
+  {
+    if (*lhs != *rhs)
+    {
+      ++difference;
+    }
+  }
+
+  return is_end_of_string(lhs) && is_end_of_string(rhs)
+    ? difference
+    : -1;
+}
